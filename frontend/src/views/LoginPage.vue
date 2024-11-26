a2a26fdc5405aa4c58aeda3366f0716863af540a
Alexey <pirogov30002@gmail.com>
2024-11-26 09:32:09 +0300

    Feature/frontend (#13)
    * add: base frontend project
    
    * add: routing with uikit (shadcn-vue)

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button';
+</script>
+
+<template>
+  <div class="auth-container">
+    <form class="auth-form">
+      <h2>Login</h2>
+      <input type="email" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <Button>Click me</Button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'LoginPage',
+};
+</script>
