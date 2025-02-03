6969e9cc36c857c51532936beb3a7f6b9135610a
Alexey <pirogov30002@gmail.com>
2025-02-03 04:05:09 +0300

    update readme

--- backend/conf/urls.py
+++ backend/conf/urls.py
@@ -36,12 +36,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/agent/", include("agent.urls")),
     path("api/users/", include("users.urls")),
-    #path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    #path(
-    #    "api/docs/",
-    #    SpectacularSwaggerView.as_view(url_name="schema"),
-    #    name="swagger-ui",
-    #),
-    #path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     re_path(r"^(?!api|admin).*", SPAView.as_view(), name="spa"),
 ]
