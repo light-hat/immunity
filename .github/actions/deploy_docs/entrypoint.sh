45aba2724886054b183acc84ef289a1980591eeb
Alexey <pirogov30002@gmail.com>
2025-07-07 00:26:13 +0300

    test autodoc again

--- .github/actions/deploy_docs/entrypoint.sh
+++ .github/actions/deploy_docs/entrypoint.sh
@@ -22,9 +22,7 @@ cd "$GITHUB_WORKSPACE"
 
 echo "Building documentation in $DOCS_DIR"
 cd "$DOCS_DIR" || exit
-sphinx-apidoc -f -o ./development/ ../../backend/api/models
-echo CHECK THIS OUT
-ls -la ./development/
+sphinx-apidoc -f -o ./development/backend ../../backend/api/models
 eval "$BUILD_COMMAND"
 
 git config --global user.name "GitHub Actions"
