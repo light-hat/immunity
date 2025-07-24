adada7469ad87f2f105803acb42c88e84f5302cf
Alexey <pirogov30002@gmail.com>
2025-07-24 16:52:33 +0300

    update: backend urls

--- backend/config/dev.py
+++ backend/config/dev.py
@@ -37,6 +37,11 @@ DATABASES = {
     }
 }
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = "media/"
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,6 +5,7 @@ URL configuration for config project.
 from os import environ
 
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -39,6 +40,8 @@ urlpatterns = [
 if ENV == "dev":
     urlpatterns += [
         path("admin/", admin.site.urls, name="admin"),
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     ]
 
 if ENV != "prod":
