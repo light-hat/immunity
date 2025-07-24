1e56206704746abeeae4d4bbfeacf975f901f922
Alexey <pirogov30002@gmail.com>
2025-07-24 16:44:57 +0300

    update: backend urls

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,6 +2,7 @@
 URL configuration for config project.
 """
 
+from os import environ
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.views import APIView
@@ -11,6 +12,8 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView,
 )
+
+ENV = environ.get("POSTGRES_DB")
 
 
 class HealthCheckView(APIView):
@@ -28,12 +31,22 @@ class HealthCheckView(APIView):
 urlpatterns = [
     path("", HealthCheckView.as_view(), name="health-check"),
     path("api/", include("api.urls"), name="api"),
-    path("admin/", admin.site.urls, name="admin"),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
+
+if ENV == "dev":
+    urlpatterns += [
+        path("admin/", admin.site.urls, name="admin"),
+    ]
+
+if ENV != "prod":
+    urlpatterns += [
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/docs/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+        ),
+    ]
