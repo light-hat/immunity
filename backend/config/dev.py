52fcdde2f2d4aa0e188a7a3d7ee387e30105f56b
Alexey <pirogov30002@gmail.com>
2025-07-24 15:20:09 +0300

    update: dev deploy script

--- backend/config/dev.py
+++ backend/config/dev.py
@@ -2,6 +2,15 @@
 Settings for DEV environment.
 """
 
-from .settings import *
+from config.settings import *
 
-# ...
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
