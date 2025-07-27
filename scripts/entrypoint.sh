77ea095ef7f0229385055c87556844f1b6a88964
Alexey <pirogov30002@gmail.com>
2025-07-27 19:02:51 +0300

    upd script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,13 +6,15 @@ if [ "$DJANGO_ENV" = "dev" ]; then
   export RELOAD="true"
 fi
 
-python manage.py makemigrations --no-input --settings=config.$DJANGO_ENV
-python manage.py migrate --no-input --settings=config.$DJANGO_ENV
-python manage.py collectstatic --no-input --settings=config.$DJANGO_ENV
+python3 manage.py makemigrations --no-input --settings=config.$DJANGO_ENV
+python3 manage.py migrate --no-input --settings=config.$DJANGO_ENV
+python3 manage.py collectstatic --no-input --settings=config.$DJANGO_ENV
+python3 manage.py initdb --settings=config.$DJANGO_ENV
 
-if [ "$DJANGO_ENV" = "staging" ]; then
-  python manage.py ...
-fi
+
+# if [ "$DJANGO_ENV" = "staging" ]; then
+#   python3 manage.py ...
+# fi
 
 exec uvicorn config.asgi:application \
   --host 0.0.0.0 \
