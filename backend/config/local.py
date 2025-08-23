64e3d74b1584d4699988d3755ffeb3595190962c
Alexey <pirogov30002@gmail.com>
2025-08-23 14:35:36 +0300

    update settings

--- backend/config/local.py
+++ backend/config/local.py
@@ -100,42 +100,42 @@ DJOSER = {
     },
 }
 
-DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: True}
+# DEBUG_TOOLBAR_CONFIG = {"SHOW_TOOLBAR_CALLBACK": lambda request: True}
 
 
-def show_toolbar(request):
-    return True
+# def show_toolbar(request):
+#     return True
 
 
-SHOW_TOOLBAR_CALLBACK = show_toolbar
+# SHOW_TOOLBAR_CALLBACK = show_toolbar
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-    str(environ.get("DEV_HOSTNAME")),
-]
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+#     str(environ.get("DEV_HOSTNAME")),
+# ]
 
-DEBUG_TOOLBAR_PANELS = [
-    "debug_toolbar.panels.history.HistoryPanel",
-    "debug_toolbar.panels.versions.VersionsPanel",
-    "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.alerts.AlertsPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "debug_toolbar.panels.redirects.RedirectsPanel",
-    "debug_toolbar.panels.profiling.ProfilingPanel",
-]
+# DEBUG_TOOLBAR_PANELS = [
+#     "debug_toolbar.panels.history.HistoryPanel",
+#     "debug_toolbar.panels.versions.VersionsPanel",
+#     "debug_toolbar.panels.timer.TimerPanel",
+#     "debug_toolbar.panels.settings.SettingsPanel",
+#     "debug_toolbar.panels.headers.HeadersPanel",
+#     "debug_toolbar.panels.request.RequestPanel",
+#     "debug_toolbar.panels.sql.SQLPanel",
+#     "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+#     "debug_toolbar.panels.templates.TemplatesPanel",
+#     "debug_toolbar.panels.alerts.AlertsPanel",
+#     "debug_toolbar.panels.cache.CachePanel",
+#     "debug_toolbar.panels.signals.SignalsPanel",
+#     "debug_toolbar.panels.redirects.RedirectsPanel",
+#     "debug_toolbar.panels.profiling.ProfilingPanel",
+# ]
 
-DEBUG_TOOLBAR_CONFIG = {
-    "RESULTS_CACHE_SIZE": 3,
-    "SHOW_COLLAPSED": False,
-    "SQL_WARNING_THRESHOLD": 100,
-}
+# DEBUG_TOOLBAR_CONFIG = {
+#     "RESULTS_CACHE_SIZE": 3,
+#     "SHOW_COLLAPSED": False,
+#     "SQL_WARNING_THRESHOLD": 100,
+# }
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
